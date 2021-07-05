5cbdfd36fd34fdadc7c7a4aa4b2ac7c21e15b469
winnie-webb <wbrown152020@gmail.com>
2021-07-05 13:25:52 -0500

    Added about us section

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,10 +49,24 @@ const HomePage = () => {
                 See anything interesting ?
               </h2>
               <p>
-                Don't worry if none of these are what you are looking for. You
-                can get anything done as long as it is an artwork
+                Don't worry if these are not what you're looking for. You can
+                get anything done as long as it is an artwork
               </p>
               <div className="artwork__askBtn btn">Contact Us</div>
+            </div>
+          </section>
+          <section className="about-us">
+            <div className="about-us__imageWrapper"></div>
+            <div className="about-us__contentWrapper">
+              <h2 className="about-us__heading">What we're about</h2>
+              <p className="about-us__content">
+                Tonyarts is currently captained by a well known artist, Anthony
+                Lname, in the St.James region of Jamaica. He can do anything
+                that has to do with creativeness. Here at Tonyarts we accept any
+                request you have and strive to do our absolute best in turning{" "}
+                <b>your reality into art</b>
+              </p>
+              <div className="about-us__btn btn">Contact Us</div>
             </div>
           </section>
         </article>
