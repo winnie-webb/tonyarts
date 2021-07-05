5e2fe5b152a830ae5ce4c2575ab293527c3384af
winnie-webb <wbrown152020@gmail.com>
2021-07-04 20:38:50 -0500

    Added basic header and hero section

---
+++ src/pages/layouts/header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "../../images/tonyarts-logos_black.png";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__logoWrapper">
+        <a href="/">
+          <img className="hero__logo" src={logo} />
+        </a>
+      </div>
+      <nav>
+        <ul className="header__nav">
+          <li>
+            <a className="header__navLinks" href="/about-us">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a className="header__navLinks" href="/contact-us">
+              Contact Us
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default Header;
