76e2e57358f99f98c29f77c215748b6683ee6bc2
winnie-webb <wbrown152020@gmail.com>
2021-07-05 19:34:03 -0500

    Added Feature to send email through contact form

--- src/pages/components/contactus.js
+++ src/pages/components/contactus.js
@@ -1,9 +1,48 @@
 import * as React from "react";
-
+import { useState } from "react";
+import axios from "axios";
 const ContactUs = () => {
+  const [serverState, setServerState] = useState({
+    submitting: false,
+    status: { ok: "", msg: "Send Message" },
+  });
+  const handleServerResponse = (ok, msg, form) => {
+    setServerState({
+      submitting: false,
+      status: { ok, msg },
+    });
+    if (ok) {
+      form.reset();
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    setServerState({
+      submitting: true,
+      status: { ok: "", msg: "Send Message" },
+    });
+    axios({
+      method: "post",
+      url: "https://getform.io/f/32962f39-f1e4-4b93-a090-22b815fa548a",
+      data: new FormData(form),
+    })
+      .then((r) => {
+        handleServerResponse(true, "Message was sent!", form);
+      })
+      .catch((r) => {
+        handleServerResponse(false, r.response.data.error, form);
+      });
+  };
   return (
     <section className="contact-us flex-column">
-      <form className="contact-us__form" method="POST">
+      <form
+        onSubmit={handleSubmit}
+        className="contact-us__form"
+        method="POST"
+        action="https://getform.io/f/32962f39-f1e4-4b93-a090-22b815fa548a"
+      >
         <h2 className="contact-us__heading heading">Get in touch</h2>
 
         <div className="inputWrapper">
@@ -33,6 +72,7 @@ const ContactUs = () => {
         </div>
       </form>
       <div className="contact-us__imageWrapper"></div>
+      <p>{serverState.status.msg}</p>
     </section>
   );
 };
